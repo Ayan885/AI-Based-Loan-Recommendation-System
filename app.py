9f71dcce1d34886f3a17a2ed507adaa1ed51bbe0
purpleafk@gmail.com <142072566+Ayan885@users.noreply.github.com>
2024-02-18 15:17:43 +0530

    some tweaks

--- app.py
+++ app.py
@@ -12,3 +12,12 @@ def hello_world():
 
 app.route('/predict', methods = ['POST', 'GET'])
 def predict():
+    init_features = [float(x) for x in request.form.values()]
+    final_features = [np.array(init_features)]
+    prediction = model.predict(final_features) # Make a prediction
+
+    return render_template('index.html', prediction_text='Predicted Species: {}'.format(prediction)) # Render the predicted result
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
